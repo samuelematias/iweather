6fb550f7a031275b2087f1a7db2d72f4f7fe35f4
Samuel Matias <hi@samuelematias.com>
2023-12-26 12:48:03 -0300

    test(mock): create mockCityStorage

---
+++ __tests__/mocks/storage/mockCityStorage.ts
@@ -0,0 +1,8 @@
+import { CityProps } from "@services/getCityByNameService";
+
+export const mockCityStorage: CityProps = {
+    id: '1',
+    name: 'Recife',
+    latitude: 123,
+    longitude: 456
+}
