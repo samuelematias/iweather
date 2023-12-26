f0a1c016513a9bb6a7622e40da46c668d2fecd66
Samuel Matias <hi@samuelematias.com>
2023-12-26 12:36:51 -0300

    test(storage): add new storage test case

--- src/libs/asyncStorage/__test__/cityStorage.test.ts
+++ src/libs/asyncStorage/__test__/cityStorage.test.ts
@@ -1,9 +1,25 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage"
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { CityProps } from "@services/getCityByNameService";
 
 describe('Storage: CityStorage', () => {
-    it("should be return null when don't have a city storaged", async () => {
+    test("should be return null when don't have a city storaged", async () => {
         const response = await getStorageCity();
 
         expect(response).toBeNull();
-    })
+    });
+
+    test('shoul be return city storaged', async () => {
+        const newCity: CityProps = {
+            id: '1',
+            name: 'Recife',
+            latitude: 123,
+            longitude: 456
+        }
+
+        await saveStorageCity(newCity)
+
+        const response = await getStorageCity()
+
+        expect(response).toEqual(newCity)
+    });
 })
