0af1b833cb52965d9ffe21fb756f7b732162f4c0
Samuel Matias <hi@samuelematias.com>
2023-12-26 10:59:28 -0300

    test(component): create empty test to SelectList

--- src/components/SelectList/__test__/SelectList.test.tsx
+++ src/components/SelectList/__test__/SelectList.test.tsx
@@ -25,4 +25,18 @@ describe('Component: SelectList', () => {
 
         expect(onPress).toHaveBeenCalledWith(data[1])
     });
+
+    test('not should be show options when data props is empty.', () => {
+        render(
+            <SelectList
+                data={[]}
+                onChange={() => {}}
+                onPress={() => {}}
+            />
+        )
+
+        const options = screen.getByTestId('options')
+
+        expect(options.children).toHaveLength(0)
+    });
 });
