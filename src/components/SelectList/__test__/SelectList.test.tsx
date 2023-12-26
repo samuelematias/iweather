edcfb19083afdbef4bb2c59e0b6ef1db1dea6323
Samuel Matias <hi@samuelematias.com>
2023-12-26 10:43:01 -0300

    test(component): implement SelectList tests

---
+++ src/components/SelectList/__test__/SelectList.test.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react-native';
+
+import { SelectList } from '@components/SelectList';
+
+describe('Component: SelectList', () => {
+    test('should be return city details selected.', () => {
+
+        const data = [
+            { id: '1', name: 'Recife', latitude: 123, longitude: 456 },
+            { id: '2', name: 'Maria Farinha', latitude: 789, longitude: 987 }
+        ]
+
+        const { getByText } = render(
+            <SelectList
+                data={data}
+                onChange={() => {}}
+                onPress={() => {}}
+            />
+        );
+
+        expect(getByText('Recife')).toBeTruthy();
+    });
+});
