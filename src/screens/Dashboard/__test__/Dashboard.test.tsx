7b77f9d7dc428288152f8d178c60b8ed749c9331
Samuel Matias <hi@samuelematias.com>
2023-12-27 08:20:13 -0300

    test(screen): improve Dashboard tests

--- src/screens/Dashboard/__test__/Dashboard.test.tsx
+++ src/screens/Dashboard/__test__/Dashboard.test.tsx
@@ -1,25 +1,28 @@
+import { mockWeatherAPIResponse } from "@__tests__/mocks/services/mockWeatherAPIResponse"
+import { render, screen, waitFor } from "@__tests__/utils/customRender"
 import { api } from "@services/api"
 import { Dashboard } from "@screens/Dashboard"
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
-import { mockWeatherAPIResponse } from "@__tests__/mocks/services/mockWeatherAPIResponse"
-import { render, screen, waitFor } from "@__tests__/utils/customRender"
 
 describe("Screen: Dashboard", () => {
-    test('should be show city weather', async () => {
-        jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
 
+    beforeAll(async () => {
         const city = {
             id: '1',
-            name: 'Paulista, BR',
+            name: 'Rio de Janeiro, BR',
             latitude: 123,
             longitude: 456
         }
 
         await saveStorageCity(city)
+    })
+
+    it('should be show city weather', async () => {
+        jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
 
         render(<Dashboard />)
 
-        const cityName = await waitFor(() => screen.findByText(city.name));
+        const cityName = await waitFor(() => screen.findByText(/rio de janeiro/i));
         expect(cityName).toBeTruthy()
     })
 })
